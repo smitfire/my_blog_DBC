c8fabc1df837b983b8a6f74f2456f2b6c8b704af
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-15 20:17:21 -0500

    My blog for prep of Phase 3

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+
+  http_basic_authenticate_with name: "nick", password: "smit", except: [:index, :show]
+
+  def new
+  @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(params[:post])
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBlog::Application.config.secret_token = 'cfa85b5186592d7c8c1b43b8e9c822050494aaf4ea208fa983474d8eef6e029fe4ab90bcd1bffb77609f80e11aa5b935ccdaec7995c2510575a0188b12e4eb7e'
